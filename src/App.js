5ad63c8718e3ee6ad953b5f777f9d1b1fe1b92ee
Ian Dewsbury <ianmichaeld@gmail.com>
2018-04-28 15:57:24 -0400

    render single canvas with react

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import P5Wrapper from 'react-p5-wrapper'
 import logo from './logo.svg';
 import './App.css';
+import sketch from './MoonPhaseView.js'
 
 class App extends Component {
   render() {
@@ -14,6 +16,7 @@ class App extends Component {
           <img alt="Sun/moon/earth illuminated orbiter" src="https://images.schoolspecialty.com/images/322573_5292A4AB7FC64C3E8886339C212C5CD2.jpg" />
           <img alt="Day to day moon phases" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpBkUdf7AHUiW0C1nZz4BJ7ZbsfJhZC4xS6k2u-L6h7M5zl0u5" />
         </p>
+        <P5Wrapper sketch={sketch} rotation={45}/>
       </div>
     );
   }
---
+++ src/MoonPhaseView.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+
+export default function sketch (p) {
+  let rotation = 0;
+
+  p.setup = function () {
+    p.createCanvas(600, 400, p.WEBGL);
+  };
+
+  p.myCustomRedrawAccordingToNewPropsHandler = function (props) {
+    if (props.rotation){
+      rotation = props.rotation * Math.PI / 180;
+    }
+  };
+
+  p.draw = function () {
+    p.background(100);
+    p.stroke('black');
+    p.push();
+    p.rotateY(rotation);
+    p.box(100);
+    p.pop();
+    rotation += .001
+  };
+};
