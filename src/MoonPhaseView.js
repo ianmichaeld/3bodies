0035836bb2f46d07174296d49cf62ee733a4cb40
Ian Dewsbury <ianmichaeld@gmail.com>
2018-04-29 18:09:13 -0400

    implement moon phases

--- src/MoonPhaseView.js
+++ src/MoonPhaseView.js
@@ -2,35 +2,24 @@ import React from 'react'
 import P5Wrapper from 'react-p5-wrapper'
 
 function sketch(p) {
-  let rotation = 0
-  var moon
-  p.preload = function() {
-  }
+  // var moon
+  let xzAngle = 0
+
   p.setup = function() {
     p.createCanvas(400, 400, p.WEBGL)
-    moon = p.loadImage('full_moon_large.jpg')
-    console.log(moon)
-  }
-
-  p.myCustomRedrawAccordingToNewPropsHandler = function(props) {
-    if (props.rotation) {
-      rotation = props.rotation * Math.PI / 180
-    }
+    // moon = p.loadImage('full_moon_large.jpg')
+    // console.log(moon)
   }
 
   p.draw = function() {
     p.background('#040623')
-    let angle = 0
-    let dirX = (p.mouseX / p.width - 0.5) * 2
-    let dirY = (p.mouseY / p.height - 0.5) * 2
-    let dirZ = (400 / p.height - 0.5) * 2
+    let dirX = 2 * p.cos(xzAngle)
+    let dirZ = 2 * p.sin(xzAngle)
     p.noStroke()
-    p.directionalLight(250, 250, 250, -dirX, 0, -dirZ)
-    // p.ambientMaterial(250)
-
-    p.texture(moon);
+    p.directionalLight(250, 250, 250, dirX, 0, dirZ)
+    p.ambientMaterial(250)
     p.sphere(100)
-    angle += 0.001
+    xzAngle += 0.005
   }
 }
 
