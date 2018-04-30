54dff0e8d5bcb63b86418ed64d096d44c3af22f4
Ian Dewsbury <ianmichaeld@gmail.com>
2018-04-29 23:18:12 -0400

    add night side to earth

--- src/EarthMoonView.js
+++ src/EarthMoonView.js
@@ -2,8 +2,8 @@ import React from 'react'
 import P5Wrapper from 'react-p5-wrapper'
 
 function sketch(p) {
-  var deg = 0
-  var deg2 = 0
+  let deg = 0
+  let deg2 = 0
 
   p.setup = function() {
     p.createCanvas(1000, 1000)
@@ -12,33 +12,41 @@ function sketch(p) {
   p.draw = function() {
     p.background('#040623')
     p.noStroke()
-    p.rectMode(p.CENTER)
     p.push()
-    p.translate(p.width / 2, p.height / 2)
-    p.rotate(deg2)
-    p.fill('blue')
-    // p.rect(0, 0, 60, 60)
-    p.ellipse(0, 0, 150)
+      p.translate(p.width / 2, p.height / 2)
+      p.rotate(deg2)
+      p.fill('#4368d8')
+      p.ellipse(0, 0, 150)
+      p.stroke('red')
+      p.strokeWeight(4)
+      p.line(0, 80, 0, 75)
+      p.line(0, 40, 0, 35)
+      p.fill('red')
+      p.ellipse(0, 85, 5)
     p.pop()
 
     p.push()
-    p.translate(p.width / 2, p.height / 2)
-    p.rotate(deg)
+      p.translate(p.width / 2, p.height / 2)
+      p.fill(50, 225)
+      p.blendMode(p.MULTIPLY)
+      p.arc(0, 0, 150, 150, 0, p.PI)
+    p.pop()
+
     p.push()
-    p.translate(p.width / 4, p.height / 4)
-    p.rotate(-deg)
-    p.fill('gray')
-    // ellipse(width/4, height/4, 30);
-    // p.rect(0, 0, 30, 30)
-    p.arc(0, 0, 80, 80, 0, p.PI)
-    p.fill('white')
-    p.arc(0, 0, 80, 80, p.PI, p.TWO_PI)
-
+      p.translate(p.width / 2, p.height / 2)
+      p.rotate(deg)
+      p.push()
+        p.translate(p.width / 4, p.height / 4)
+        p.rotate(-deg)
+        p.fill('#3a3a37')
+        p.arc(0, 0, 80, 80, 0, p.PI)
+        p.fill('white')
+        p.arc(0, 0, 80, 80, p.PI, p.TWO_PI)
+      p.pop()
     p.pop()
-    p.pop()
 
-    deg += 0.001
-    deg2 += 0.028
+    deg -= 0.001
+    deg2 -= 0.028
   }
 }
 
