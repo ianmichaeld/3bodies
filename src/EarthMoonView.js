56d9929508b4f0eb7888172e40d1083a741fb887
Ian Dewsbury <ianmichaeld@gmail.com>
2018-04-29 22:11:26 -0400

    refactor EarthMoon view to 2d

--- src/EarthMoonView.js
+++ src/EarthMoonView.js
@@ -6,27 +6,39 @@ function sketch(p) {
   var deg2 = 0
 
   p.setup = function() {
-    p.createCanvas(1000, 1000, p.WEBGL)
+    p.createCanvas(1000, 1000)
   }
 
   p.draw = function() {
     p.background('#040623')
     p.noStroke()
-    p.directionalLight(250, 250, 250, 0, -4, 0)
+    p.rectMode(p.CENTER)
     p.push()
-      p.ambientMaterial('blue')
-      p.sphere(100)
-      p.rotateZ(deg2)
+    p.translate(p.width / 2, p.height / 2)
+    p.rotate(deg2)
+    p.fill('blue')
+    // p.rect(0, 0, 60, 60)
+    p.ellipse(0, 0, 150)
     p.pop()
+
     p.push()
-    p.translate(-p.width/4, -p.height/4)
-    p.ambientMaterial('#7c7c7a')
-      p.sphere(30)
-      p.translate(p.width/4, p.height/4)
-      p.rotateZ(deg)
+    p.translate(p.width / 2, p.height / 2)
+    p.rotate(deg)
+    p.push()
+    p.translate(p.width / 4, p.height / 4)
+    p.rotate(-deg)
+    p.fill('gray')
+    // ellipse(width/4, height/4, 30);
+    // p.rect(0, 0, 30, 30)
+    p.arc(0, 0, 80, 80, 0, p.PI)
+    p.fill('white')
+    p.arc(0, 0, 80, 80, p.PI, p.TWO_PI)
+
     p.pop()
-    deg += 0.001;
-	  deg2 += 0.028;
+    p.pop()
+
+    deg += 0.001
+    deg2 += 0.028
   }
 }
 
